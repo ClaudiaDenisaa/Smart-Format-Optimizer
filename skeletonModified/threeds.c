eadfb8a60e9a95a04365cbe4f1c99565d054864e
SaraB03 <118679724+SaraB03@users.noreply.github.com>
2026-04-21 17:20:39 +0300

    Add files via upload

---
+++ skeletonModified/threeds.c
@@ -0,0 +1,84 @@
+#include <stddef.h>
+#include <errno.h>
+#include <pthread.h>
+//#include <ncurses.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <libconfig.h>
+
+void *unix_main (void *args) ;
+void *inet_main (void *args) ;
+void *soap_main (void *args) ;
+
+// WINDOW *mainwnd ;
+#define UNIXSOCKET "/tmp/unixds"
+#define SOAPPORT   18082
+
+pthread_mutex_t curmtx = PTHREAD_MUTEX_INITIALIZER ;
+
+int main () {
+  config_t cfg;
+  config_init(&cfg);
+  int iport, sport ;
+
+  pthread_t unixthr, /* UNIX Thread: the UNIX server component */
+	inetthr,     /* INET Thread: the INET server component */ 
+	soapthr ;     /* SOAP Thread: the SOAP server component */
+//	workerthr ;  /* The Worker Thread: use it for WORK tasks (various) */
+
+  if (!config_read_file(&cfg, "server.cfg")) {
+    fprintf(stderr, "Eroare la citire config: %s la linia %d\n", 
+            config_error_text(&cfg), config_error_line(&cfg));
+    config_destroy(&cfg);
+    return -1;
+  }
+
+
+  if (!config_lookup_int(&cfg, "server_config.port", &iport)) {
+      fprintf(stderr, "Avertisment: Nu am gasit portul, folosesc default 18081\n");
+      iport = 18081; 
+  } else {
+      fprintf(stderr, "[CONFIG] Serverul va porni pe portul %d\n", iport);
+  }
+
+ 
+/*
+  mainwnd = initscr () ;
+  noecho () ;
+  cbreak () ;
+  if (has_colors()) start_color () ;
+  init_pair (1, COLOR_RED, COLOR_BLACK) ;
+//  refresh () ;
+*/
+  unlink (UNIXSOCKET) ;
+
+  pthread_create (&unixthr, NULL, unix_main, UNIXSOCKET) ; /* Transmite SOCKET-ul utilizat */
+  pthread_create (&inetthr, NULL, inet_main, &iport) ;
+
+  sport = SOAPPORT ;
+  pthread_create (&soapthr, NULL, soap_main, &sport) ;
+/*
+  pthread_create (&workerthr, NULL, work_main, NULL) ;
+    Implementarea firului de lucru:
+	acest fir va rezolva sarcinile de lucru uzuale, bazate pe cereri INET/SOAP	
+	firul de lucru proceseaza cererile din coada sau este suspendat (vezi mai jos)
+	ar putea utiliza mecanismul 'condition variable': orice cerere noua este semnalata catre firul de lucru 
+		prin acest mecanism.
+ */
+  pthread_join (unixthr, NULL) ;
+  pthread_join (inetthr, NULL) ;
+  pthread_join (soapthr, NULL) ;
+
+/*
+  getch () ;
+  endwin () ;
+  */
+  unlink (UNIXSOCKET) ;
+  config_destroy(&cfg);
+  return 0 ;
+}
+
